a1583e3a41a692d9dd7515c39c84c0dd71a64a9c
wuli(=•ェ•=) <72075383+SherlockFrailty@users.noreply.github.com>
2021-01-09 22:56:40 +0800

    Add files via upload

---
+++ ojquestion/,,.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double l,w;
+	double area,girth;
+	cout<<"ÊäÈë¾ØÐÎ³¤¿í"; 
+	cin>>l>>w;
+	area=l*w;
+	girth=2*(l+w);
+	cout<<"area="<<area<<",girth="<<girth<<endl;
+	return 0; 
+}
